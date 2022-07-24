3cc0ce7b8e69159f9468b417a34e4558d8100666
unknown <visaolangle00@gmail.com>
2022-07-24 15:28:44 +0700

    part5

--- lib/data/services/register/register_service.dart
+++ lib/data/services/register/register_service.dart
@@ -4,9 +4,10 @@ import 'package:dio/dio.dart';
 import '../dio_manager.dart';
 import 'model/register_request_model.dart';
 import 'model/register_response_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class RegisterService {
-  Future<RegisterResponseModel> register(RegisterRequestModel registerRequestModel);
+  Future register(RegisterRequestModel registerRequestModel);
 }
 
 class RegisterServiceImp extends RegisterService {
@@ -15,13 +16,24 @@ class RegisterServiceImp extends RegisterService {
   RegisterServiceImp(DioManager _dioManager) : _dio = _dioManager.dio;
 
   @override
-  Future<RegisterResponseModel> register(RegisterRequestModel registerRequestModel) {
-    return _dio
-        .post(
-      "http://192.168.1.4/flutter/flutter_notegetx_mini/register.php",
-      //"flutter/flutter_notegetx_mini/register.php",
-      data: registerRequestModel.toJson(),
-    )
-        .then((response) => registerResponseModelFromJson(response.data));
+  Future register(RegisterRequestModel registerRequestModel) async {
+    // return await _dio
+    //     .post(
+    //       "notebook/register.php",
+    //       data: registerRequestModel.toJson(),
+    //     )
+    //     .then((response) => registerResponseModelFromJson(response.data));
+
+    const String _baseUrl = 'http://192.168.1.4/flutter/flutter_notegetx_mini/register.php';
+    var _url = Uri.parse(_baseUrl);
+
+    final response = await http.post(_url, body: registerRequestModel.toJson());
+
+    if (response.statusCode == 200) {
+      print('Kayıt başarılı');
+      return registerResponseModelFromJson(response.body);
+    } else {
+      return '';
+    }
   }
 }
