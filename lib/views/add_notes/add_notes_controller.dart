41804b5da2b7910b07aeb7e98beb8d1bba960c69
unknown <visaolangle00@gmail.com>
2022-07-24 15:37:19 +0700

    part11

--- lib/views/add_notes/add_notes_controller.dart
+++ lib/views/add_notes/add_notes_controller.dart
@@ -1,4 +1,8 @@
 
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:flutter/material.dart';
 
-class AddNotesController extends GetxController {}
+class AddNotesController extends GetxController {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+}
